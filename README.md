1a85d73d941528cb6ef7a1a503db2d78a88065ce
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2022-10-16 17:37:47 +0300

    Fixed some bugs, created some more

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Calculator
-Calculator Project i didnt to finish Odin Foundations
+Calculator Project i did to finish Odin Project Foundations
+
+Things i learned:
+
+That i should commit on git after every session of working on a project, even if it is just a small improvement.
+Taking a break from the project is usefull (if it is not too long), to let the brain get some new ideas.
+To addEventListener only to a specific element, not to the whole document (like i used to on smaller projects).
+Also it was the first time i used obj.forEach. 
+Trying to brake my own app is good at helping bug fixes and not to test the app with only simple inputs. 
+
+
+Exercise is key#.
--- javascript.js
+++ javascript.js
@@ -14,15 +14,146 @@ const divisonNumbers = (a,b) => {
     return a/b;
 }
 
-const operaton = (sign,num1,num2) =>{
+const operaton = (sign,currentValue,operationResult) =>{
+    console.log('==== OPERATOR =====');
+    console.log(`currentValue: ${currentValue}`);
+    console.log(`operationResult: ${operationResult}`);
+    console.log(`sign: ${sign}`);
+    console.log('==============');
     switch(sign){
-        case '+': sumNumbers(num1,num2);
-        break;
-        case '-': substractionNumbers(num1,num2);
-        break;
-        case '*': multiplicationNumbers(num1,num2);
-        break;
-        case '/': divisonNumbers(num1,num2);
-        break;
+        case '+': return sumNumbers(operationResult,currentValue);
+        case '-': return substractionNumbers(operationResult,currentValue);
+        case '*': return multiplicationNumbers(currentValue,operationResult);
+        case '/': return divisonNumbers(operationResult,currentValue);
     }
 }
+
+const display = document.querySelector('.output-display');
+const history = document.querySelector('.past-operations');
+const errorDisplay = document.querySelector('.error-info');
+let displayString = '';
+let operator = '';
+let result;
+let currentValue, operationResult=0, count=1;
+let isLastPressedSign = false;
+
+const clickDeleteHandler = (e) => {
+    const choosing = e.target.dataset.value;
+    isLastPressedSign = false;
+    if(!choosing) return;
+    if(e.target.dataset.value === 'Del')
+    {
+        displayString = '';
+        display.textContent = displayString;
+        currentValue = null;
+        operationResult = null;
+        count=1;
+        operator='';
+        history.textContent = displayString;
+    }
+
+    if(e.target.dataset.value === 'C' && displayString.length > 0)
+    {
+        let string = '';
+        string = displayString.slice(0,-1);
+        displayString = string;
+        display.textContent = displayString;
+    }
+}
+
+const numbersClickHandler = (e) =>{
+    const choosing = e.target.dataset.value;
+    isLastPressedSign = false;
+    if(!choosing) return;
+    if(e.target.dataset.value ==='0' && displayString==='0') return;
+    if(choosing ==='.' && displayString.includes('.'))return;
+    if(operator==='=' && choosing!='=')
+    {
+        history.textContent='';
+        displayString='';
+        operator='';
+    }
+    if(displayString.length>=10)
+    {
+        errorDisplay.textContent = 'We can only do 10 figures at a time :(';
+        return;
+    }
+    displayString += choosing;
+    display.textContent = displayString;
+    history.textContent += choosing;
+    errorDisplay.textContent = '';
+    currentValue = Number(displayString);
+}
+
+const operationsClickHandler = (e) =>{
+    const choosing = e.target.dataset.value;
+    if(!choosing) return;
+    /*if(count===1)
+    {
+        currentValue = parseFloat(displayString);
+        count++;
+    }
+    else {
+        operationResult = parseFloat(displayString);
+    }*/
+    operator = e.target.dataset.value;
+    if ((currentValue === 0 || operationResult === 0) && operator === '/') {
+        display.textContent = 'Nice try!';
+    }
+    else if(!isLastPressedSign){
+        console.log(currentValue,operationResult);
+        result = operaton(operator, currentValue, operationResult);
+        console.log(result);
+        display.textContent = result;
+        operationResult = result;
+        currentValue = 0;
+    }
+    if(!isLastPressedSign){
+        history.textContent += operator;
+    }
+    else{
+        history.textContent = history.textContent.slice(0,-1) + operator;
+    }
+    isLastPressedSign = true;
+    displayString = '';
+    display.textContent = e.target.dataset.value;
+}
+
+const equalClickHandler = (e) =>{
+    const choosing = e.target.dataset.value;
+    isLastPressedSign = false;
+    if(!choosing) return;
+    if((currentValue===0 || operationResult===0) && operator==='/')
+    {
+        display.textContent = 'Nice try!';
+    }
+    if(currentValue!=undefined && operationResult!=undefined)
+    {
+        console.log(currentValue,operationResult);
+        result = operaton(operator,currentValue,operationResult);
+        display.textContent = result;
+        operationResult = result;
+        console.log(operationResult);
+        currentValue = 0;
+    }
+}
+
+const equalButton = document.querySelector('.equal');
+
+equalButton.addEventListener('click',equalClickHandler);
+
+const butoane = document.querySelectorAll('.number');
+butoane.forEach((el,index)=>{
+    el.addEventListener('click',numbersClickHandler);
+})
+
+const deleteButtons = document.querySelectorAll('.delete');
+deleteButtons.forEach((el,index)=>{
+    el.addEventListener('click',clickDeleteHandler);
+})
+
+const operationButtons = document.querySelectorAll('.keyboard-operator');
+operationButtons.forEach((el,index)=>{
+    el.addEventListener('click',operationsClickHandler)
+})
+
